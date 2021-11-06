687ad75dfed7629e95e28fab55c3caea98268b6b
Daemon-L <lit.dmitriy.ua@gmail.com>
2021-11-06 23:11:47 +0200

    Добавляет начальные данные для задания 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,9 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector(".form");
+form.addEventListener("submit", onFormData);
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +12,14 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+
+
+
+createPromise(position, delay)
+  .then(({ position, delay }) => {
+    Notiflix.Notify.failure(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
