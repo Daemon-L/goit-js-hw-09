0ffb78a4ea675143e88a99cca16687fe3c3da39d
Daemon-L <lit.dmitriy.ua@gmail.com>
2021-11-06 19:32:38 +0200

    начало задания 2

---
+++ src/js/02-timer.js
@@ -0,0 +1,66 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const refs = {
+    startBtn: document.querySelector("button[data-start]"),
+    input: document.querySelector("#datetime-picker"),
+    days: document.querySelector("span[data-days]"),
+    hours: document.querySelector("span[data-hours]"),
+    minutes: document.querySelector("span[data-minutes]"),
+    seconds: document.querySelector("span[data-seconds]"),
+    notification: document.querySelector(".notification"),
+};
+
+let selectedTime = null;
+let currentTime = null;
+let timerId = null;
+
+refs.startBtn.setAttribute('disabled', true);
+refs.startBtn.addEventListener("click", onStartClick)
+
+//  объект параметров для flatpickr  ********************
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr("#datetime-picker", options);
+// ******************************************************
+
+// функция добавляет '0'  *********************************
+function addLeadingZero(value) {
+return String(value).padStart(2, "0");
+}
+// ******************************************************
+
+
+// готовая функция для подсчета значений ***************
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+// ****************************************************************************************
