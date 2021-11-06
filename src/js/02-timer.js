292b9eaf78b239349faa3c7f4c3066dd50774204
Daemon-L <lit.dmitriy.ua@gmail.com>
2021-11-06 22:57:24 +0200

    Добавляет решение задачи 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -15,52 +15,72 @@ const refs = {
 let selectedTime = null;
 let currentTime = null;
 let timerId = null;
-
 refs.startBtn.setAttribute('disabled', true);
-refs.startBtn.addEventListener("click", onStartClick)
+refs.startBtn.addEventListener("click", startTimer)
 
-//  объект параметров для flatpickr  ********************
 const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        checkDates(selectedDates);
+    },
 };
-
 flatpickr("#datetime-picker", options);
-// ******************************************************
 
-// функция добавляет '0'  *********************************
+function checkDates(selectedDates) {
+    selectedTime = selectedDates[0].getTime();
+    currentTime = Date.now();
+    if (selectedTime < currentTime) {
+       Notiflix.Notify.failure('Please choose a date in the future');
+    } else {
+      refs.startBtn.disabled = false;}
+}
+
+function startTimer() {
+    refs.input.setAttribute('disabled', true);
+    refs.startBtn.setAttribute('disabled', true);
+    
+    timerId = setInterval(() => {
+        currentTime = Date.now();
+        const deltaTime = selectedTime - currentTime;
+        if (selectedTime <= currentTime) {
+          clearInterval(timerId);
+          Notiflix.Notify.failure('BANG');
+          return;
+        } 
+        const time = convertMs(deltaTime);
+        updateTime(time)
+    }, 1000)
+}
+
+function updateTime({ days, hours, minutes, seconds }) {
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.minutes.textContent = `${minutes}`;
+  refs.seconds.textContent = `${seconds}`;
+}
+
+// добавляет '0'  *********************************
 function addLeadingZero(value) {
 return String(value).padStart(2, "0");
 }
-// ******************************************************
 
-
-// готовая функция для подсчета значений ***************
+// подсчет значений *********************************
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-
   // Remaining days
-  const days = Math.floor(ms / day);
+  const days = addLeadingZero(Math.floor(ms / day));
   // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
   // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
   // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
   return { days, hours, minutes, seconds };
 }
-// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
-
-// ****************************************************************************************
